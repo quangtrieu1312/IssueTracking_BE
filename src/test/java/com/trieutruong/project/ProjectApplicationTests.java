72c344615b5582e84067c4b5b621674e2d6a8ab1
Trieu Truong <quangtrieu1312@gmail.com>
2020-10-20 23:41:58 +0700

    small fix

--- src/test/java/com/trieutruong/project/ProjectApplicationTests.java
+++ src/test/java/com/trieutruong/project/ProjectApplicationTests.java
@@ -1,4 +1,4 @@
-package com.trieutruong.webpage;
+package com.trieutruong.project;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class WebpageApplicationTests {
+public class ProjectApplicationTests {
 
 	@Test
 	public void contextLoads() {
